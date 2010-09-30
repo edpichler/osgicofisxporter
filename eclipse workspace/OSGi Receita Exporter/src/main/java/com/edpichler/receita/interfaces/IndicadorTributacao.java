2d7b119b97afe96f137ee0851d6ef8ebe6c2a7f0
edpichler <edpichler@localhost>
2010-09-30 11:03:03 -0300

    Alterado a pedido do Eldoney.

--- eclipse workspace/OSGi Receita Exporter/src/main/java/com/edpichler/receita/interfaces/IndicadorTributacao.java
+++ eclipse workspace/OSGi Receita Exporter/src/main/java/com/edpichler/receita/interfaces/IndicadorTributacao.java
@@ -1,8 +1,5 @@
 package com.edpichler.receita.interfaces;
 
-public enum IndicadorTributacao {
-	// 17 Indicador de Tributa��o do IPI 183 1 C Nas sa�das, preencher com:
-	// "1"=TRIBUTADO; "2"=ISENTO OU N�O TRIBUTADO; "3"=OUTRAS. Nas entradas,
-	// preencher com: "1"=RECUPERA IPI; "2"=ISENTO OU N�O TRIBUTADO; "3"=OUTRAS.
+public enum IndicadorTributacao {	
 	TRIBUTADO, ISENTO_OU_NAO_TRIBUTADO, OUTRAS, RECUPERA_IPI
 }
--- eclipse workspace/OSGi Receita Exporter/src/main/java/com/edpichler/receita/services/impl/GeradorItensMercadoriasServicosNFEmitidosPJ.java
+++ eclipse workspace/OSGi Receita Exporter/src/main/java/com/edpichler/receita/services/impl/GeradorItensMercadoriasServicosNFEmitidosPJ.java
@@ -13,7 +13,7 @@ import com.edpichler.receita.interfaces.ItensMercadoriasServicosNFEmitidosPJ;
 import com.edpichler.receita.services.Constantes;
 import com.edpichler.receita.services.IGerador;
 
-public class GeradorItensMercadoriasServicosNFEmitidosPJ  extends GeradorBase
+public class GeradorItensMercadoriasServicosNFEmitidosPJ extends GeradorBase
 		implements IGerador {
 	private static String nomeArquivo = "4_3_2_arquivo_de_itens_de_mercadorias_servicos_notas_fiscais_saida_entrada_emitidas_pessoa_juridica.txt";
 
@@ -31,90 +31,85 @@ public class GeradorItensMercadoriasServicosNFEmitidosPJ  extends GeradorBase
 	}
 
 	private String formatar(ItensMercadoriasServicosNFEmitidosPJ param) {
-		
+
 		StringBuilder st = new StringBuilder();
-		
+
 		EntradaSaida entSai = param.getIndicadorMovimento();
-		if(entSai == EntradaSaida.ENTRADA){
-			st.append(formatarString("E", 1));	
-		}else{
+		if (entSai == EntradaSaida.ENTRADA) {
+			st.append(formatarString("E", 1));
+		} else {
 			st.append(formatarString("S", 1));
-		}	
-		
+		}
+
 		st.append(formatarString(param.getModeloDocumento(), 2));
-		st.append(formatarString(param.getSerieSubserieDocumento(), 5));		
+		st.append(formatarString(param.getSerieSubserieDocumento(), 5));
 		st.append(formatarNumero(param.getNumeroDocumento(), 9, 0));
-		st.append(formatarData(param.getDataEmissaoDocumento()));		
+		st.append(formatarData(param.getDataEmissaoDocumento()));
 		st.append(formatarNumero(param.getNumeroDoItem(), 3, 0));
 		st.append(formatarString(param.getCodigoMercadoriaServico(), 20));
-		
+
 		st.append(formatarString(param.getDescricaoComplementar(), 45));
 		st.append(formatarString(param.getCodigoFiscalOperacao(), 4));
 		st.append(formatarString(param.getCodigoNaturezaOperacao(), 6));
 		st.append(formatarString(param.getClassificaoFiscalMercadoria(), 8));
-		
+
 		st.append(formatarNumero(param.getQuantidade(), 17, 3));
-		
+
 		st.append(formatarString(param.getUnidade(), 3));
 		st.append(formatarNumero(param.getValorUnitario(), 17, 4));
 
 		st.append(formatarNumero(param.getValorTotalDoItem(), 17, 2));
 		st.append(formatarNumero(param.getValorDescontoItem(), 17, 2));
-		
-		
-		//17 Indicador de Tributação do IPI 183 1 C Nas saídas, preencher com:
-		//"1"=TRIBUTADO; "2"=ISENTO OU NÃO TRIBUTADO; "3"=OUTRAS. Nas entradas,
-		//preencher com: "1"=RECUPERA IPI; "2"=ISENTO OU NÃO TRIBUTADO; "3"=OUTRAS.
+
+		// 17 Indicador de Tributação do IPI 183 1 C Nas saídas, preencher com:
+		// "1"=TRIBUTADO; "2"=ISENTO OU NÃO TRIBUTADO; "3"=OUTRAS. Nas entradas,
+		// preencher com: "1"=RECUPERA IPI; "2"=ISENTO OU NÃO TRIBUTADO;
+		// "3"=OUTRAS.
 		IndicadorTributacao ipi = param.getIndicadorTributacaoIpi();
-		//codificado para fácil entendimento.
-		if(entSai == EntradaSaida.SAIDA){
-			if(ipi == IndicadorTributacao.TRIBUTADO){
-				st.append(formatarString("1", 1));
-			}else if(ipi == IndicadorTributacao.ISENTO_OU_NAO_TRIBUTADO){
-				st.append(formatarString("2", 1));
-			}else{
-				st.append(formatarString("3", 1));
-			}
-		}else{
-			if(ipi == IndicadorTributacao.RECUPERA_IPI){
-				st.append(formatarString("1", 1));
-			}else if(ipi == IndicadorTributacao.ISENTO_OU_NAO_TRIBUTADO){
-				st.append(formatarString("2", 1));
-			}else{
-				st.append(formatarString("3", 1));
-			}			
-		}	
+		// codificado para fácil entendimento.
+		if (ipi == IndicadorTributacao.TRIBUTADO
+				|| ipi == IndicadorTributacao.RECUPERA_IPI) {
+			st.append(formatarString("1", 1));
+		} else if (ipi == IndicadorTributacao.ISENTO_OU_NAO_TRIBUTADO) {
+			st.append(formatarString("2", 1));
+		} else {
+			st.append(formatarString("3", 1));
+		}
+
 		st.append(formatarNumero(param.getAliquotaIpi(), 5, 2));
 		st.append(formatarNumero(param.getBaseCalculoIpi(), 17, 2));
-		
+
 		st.append(formatarNumero(param.getValorIpi(), 17, 2));
 		st.append(formatarString(param.getSituacaoTributariaEstadual(), 3));
 
 		IndicadorTributacao indiIcms = param.getIndicadorTributarioIcms();
-		
-		if(indiIcms == IndicadorTributacao.TRIBUTADO){
+
+		if (indiIcms == IndicadorTributacao.TRIBUTADO
+				|| indiIcms == IndicadorTributacao.RECUPERA_IPI) {
 			st.append(formatarString("1", 1));
-		}else if(indiIcms == IndicadorTributacao.ISENTO_OU_NAO_TRIBUTADO){
+		} else if (indiIcms == IndicadorTributacao.ISENTO_OU_NAO_TRIBUTADO) {
 			st.append(formatarString("2", 1));
-		}else{
+		} else {
 			st.append(formatarString("3", 1));
-		}			
+		}
+		
 		st.append(formatarNumero(param.getAliquotaIcms(), 5, 2));
 		st.append(formatarNumero(param.getBaseCalculoIcmsProprio(), 17, 2));
 		st.append(formatarNumero(param.getValorIcmsProprio(), 17, 2));
-		
-		st.append(formatarNumero(param.getBaseCalculoIcmsSubsituicaoTributaria(), 17, 2));
-		st.append(formatarNumero(param.getValorIcmsSubstituicaoTributaria(), 17, 2));
-		
-		if(param.getIndicadorMovimentacaoFisicaMercadoria()){
+
+		st.append(formatarNumero(param
+				.getBaseCalculoIcmsSubsituicaoTributaria(), 17, 2));
+		st.append(formatarNumero(param.getValorIcmsSubstituicaoTributaria(),
+				17, 2));
+
+		if (param.getIndicadorMovimentacaoFisicaMercadoria()) {
 			st.append(formatarString("S", 1));
-		}else{
+		} else {
 			st.append(formatarString("N", 1));
 		}
-		
-		st.append(formatarString(param.getCodigoSituacaoTributariaIpi(), 2 ));  
-		
-		
+
+		st.append(formatarString(param.getCodigoSituacaoTributariaIpi(), 2));
+
 		return st.toString();
 	}
 }
